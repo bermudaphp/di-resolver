2562440d2c0c6ef6fb98f7227bb15c38bf3efb73
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2025-04-10 13:14:40 +0200

    Update ParameterResolverInterface.php

--- src/ParameterResolverInterface.php
+++ src/ParameterResolverInterface.php
@@ -6,5 +6,22 @@ use Psr\Http\Message\ServerRequestInterface;
 
 interface ParameterResolverInterface
 {
-    public function resolve(\ReflectionParameter $parameter, array $params = []):? array ;
+    /**
+     * @param \ReflectionParameter $parameters
+     * @param array $params
+     * @return array<string, mixed>
+     * The method returns an array of function parameters, 
+     * where the keys will be the names of parameters
+     */
+    public function resolve(array $parameters, array $params = []): array;
+    
+    /**
+     * @param ReflectionParameter $parameter
+     * @param array $params
+     * The method returns an array where the key “0” will be the name of the parameter
+     * and the key “1” will be its value
+     * @return array{0: string, 1: mixed}
+     * @throws ResolverException
+     */
+    public function resolveParameter(\ReflectionParameter $parameter, array $params = []):? array ;
 }
