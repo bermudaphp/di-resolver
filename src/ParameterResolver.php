86d2e40ef1ba46ca42f81af72629b1823ee6d64e
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2025-04-10 13:47:09 +0200

    Update ParameterResolver.php

--- src/ParameterResolver.php
+++ src/ParameterResolver.php
@@ -2,15 +2,16 @@
 
 namespace Bermuda\ParameterResolver\Resolver;
 
-use Psr\Container\ContainerInterface;
 use ReflectionParameter;
+use Psr\Container\ContainerInterface;
 use Psr\Http\Message\ServerRequestInterface;
+
 use function Bermuda\Config\conf;
 
-final class ParameterResolver implements ParameterResolverInterface
+final class ParameterResolver
 {
     /**
-     * @var ParametrResolverInterface[]
+     * @var ParameterResolverInterface[]
      */
     private array $resolvers;
 
@@ -19,23 +20,55 @@ final class ParameterResolver implements ParameterResolverInterface
         foreach ($resolvers as $resolver) $this->addResolver($resolver);
     }
 
+    /**
+     * @param ParameterResolverInterface $resolver
+     * If $prepend true, then add the resolver to the beginning of the queue
+     * @param bool $prepend
+     * @return void
+     */
     public function addResolver(ParameterResolverInterface $resolver, bool $prepend = false): void
     {
         $prepend ? array_unshift($this->resolvers, $resolver) : $this->resolvers[] = $resolver;
     }
 
     /**
+     * @param \ReflectionParameter $parameters
+     * @param array<string, $params> $params
+     *  The method returns an array of function parameters,
+     *  where the keys will be the names of parameters
      * @return array<string, mixed>
+     * An exception will be thrown if the parameter cannot be resolved
+     * @throws ResolverException
      */
-    public function resolve(ReflectionParameter $parameter, array $params = []): array
+    public function resolve(array $parameters, array $params = []): array
+    {
+        $resolved = [];
+        foreach ($this->parameters as $parameter) {
+            $pair = $this->resolveParameter($parameter, $params);
+            $resolved[$pair[0]] = $pair[1];
+        }
+
+        return $resolved;
+    }
+
+    /**
+     * @param ReflectionParameter $parameter
+     * @param array $params
+     * The method returns an array where the key “0” will be the name of the parameter
+     * and the key “1” will be its value
+     * @return array{0: string, 1: mixed}
+     * An exception will be thrown if the parameter cannot be resolved
+     * @throws ResolverException
+     */
+    public function resolveParameter(ReflectionParameter $parameter, array $params = []): array
     {
         foreach ($this->resolvers as $resolver) {
             $pair = $resolver->resolve($parameter, $params);
-            if ($pair !== null) return $pair;
+            if ($pair) return $pair;
         }
 
         if (array_key_exists($parameter->getName(), $params)) {
-            return $params[$parameter->getName()];
+            return [$parameter->getName() => $params[$parameter->getName()]];
         }
 
         if ($parameter->isDefaultValueAvailable()) {
@@ -49,13 +82,12 @@ final class ParameterResolver implements ParameterResolverInterface
 
     public static function createFromContainer(ContainerInterface $container): self
     {
-        $resolver = new self();
+        $self = new self();
         foreach (conf($container)->get(ConfigProvider::CONFIG_KEY_RESOLVERS, []) as $resolver) {
-            $resolver->resolve($container->get($resolver));
+            $self->addResolver($container->get($resolver));
         }
 
-        $resolver->resolve(new ContainerResolver($container));
-
-        return $resolver;
+        $self->addResolver(new ContainerResolver($container));
+        return $self;
     }
 }
