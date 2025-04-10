ea296a6b1d6dfd2c476d6dbec66745fa7711f794
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2025-04-10 12:10:36 +0200

    Create ParametrResolver.php

---
+++ src/ParametrResolver.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Bermuda\Reflection\Resolver;
+
+use ReflectionParameter;
+use Psr\Http\Message\ServerRequestInterface;
+
+final class ParametrResolver implements ParametrResolverInterface
+{
+    /**
+     * @var ParametrResolverInterface[]
+     */
+    private array $resolvers;
+
+    public function __construct(iterable $resolvers = [])
+    {
+        foreach ($resolvers as $resolver) $this->addResolver($resolver);
+    }
+
+    public function addResolver(ParametrResolverInterface $resolver): void
+    {
+        $this->resolvers[] = $resolver;
+    }
+
+    /**
+     * @return array<string, mixed>
+     */
+    public function resolve(ReflectionParameter $parameter, array $params = []): array
+    {
+        foreach ($this->resolvers as $resolver) {
+            $pair = $resolver->resolve($parameter, $params);
+            if ($pair !== null) return $pair;
+        }
+
+        if (array_key_exists($parameter->getName(), $params)) {
+            return $params[$parameter->getName()];
+        }
+
+        if ($parameter->isDefaultValueAvailable()) {
+            return [$parameter->getName() => $parameter->getDefaultValue()];
+        }
+
+        if ($parameter->allowsNull()) return [$parameter->getName(), null];
+
+        throw ResolverException::createFromParameter($parameter);
+    }
+}
