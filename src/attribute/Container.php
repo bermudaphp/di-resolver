6b8b37fae29ac211caf72a1345b370d414d22cc1
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2025-04-10 12:21:03 +0200

    Create Container.php

---
+++ src/attribute/Container.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Bermuda\ParameterResolver\Attribute;
+
+use Psr\Container\ContainerExceptionInterface;
+use Psr\Container\ContainerInterface;
+use Psr\Container\NotFoundExceptionInterface;
+
+#[\Attribute(\Attribute::TARGET_PARAMETER)] class Container
+{
+    public function __construct(
+        public readonly string $id,
+    ) {
+    }
+}
