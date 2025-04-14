cfd711b1a04fdeeb8ad71f28b33ca4e8c02f020a
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2025-04-15 01:30:01 +0200

    Update Config.php

--- src/attribute/Config.php
+++ src/attribute/Config.php
@@ -9,7 +9,9 @@ use Psr\Container\NotFoundExceptionInterface;
 #[\Attribute(\Attribute::TARGET_PARAMETER)]class Config
 {
     public function __construct(
-        public readonly string|array $path,
-        public readonly string $configKey = 'config',
+        public readonly string $path,
+        public readonly bool $explodeDots = true,
     ) {}
+
+    public static string $key = 'config';
 }
