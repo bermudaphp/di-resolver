d21c041eef7b6b3abafaf825298f196dade45402
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2025-05-01 17:56:50 +0200

    Add files via upload

---
+++ src/parameter/ParameterResolutionExceptionInterface.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Bermuda\ParameterResolver;
+
+interface ParameterResolutionExceptionInterface extends \Throwable
+{
+    public ReflectionParameter $parameter {
+        get;
+    }
+
+    public array $providedParameters {
+        get;
+    }
+
+    public array $resolvedParameters {
+        get;
+    }
+}
